08b9a1807e9f809f6e1b4f151967b8c23b8305f2
santosmatheus2629-sys <santosmatheus2629@gmail.com>
2026-06-02 20:58:21 -0400

    Add files via upload

---
+++ aula-front-main/script.js
@@ -0,0 +1,61 @@
+// let aluno={
+//     saldo_conta :null,
+//     carro : {
+//         cor : cinza,
+//         marca: 'chevrollet',
+//         modelo : 'onix',
+//         ano : 2017,
+//         materias: ['web', 'banco', 'exten']
+//     },
+//     for (materias of aluno.materias) {
+//         console.log(materia)
+//     }
+//     nome : matheus,
+//     idade : 21,
+//     casado : true,
+
+// }
+
+// console.log(aluno);
+// console.log(aluno.nome);
+// console.log(aluno.idade);
+// console.log(aluno.casado);
+// console.log(aluno.saldo_conta);
+// console.log(aluno.carro.cor);
+// console.log(aluno.materias);
+
+// let chamada = [
+//     { 
+//         ra:1,
+//         nome:'antony'
+//     },
+//     {
+//         ra:2,
+//         nome:'antoni'
+//     },
+//     {
+//         ra:3,
+//         nome: 'davi'
+//     },
+// ]
+
+// for (aluno of chamada) {
+//     console.log(aluno.ra, aluno.nome)
+// }
+
+// let retorno = '{"nome":"willian","idade":23}'
+// console.log(retorno);
+
+// let cadastro = JSON.parse(retorno);
+// console.log(cadastro);
+
+// let cadastro = {
+//     nome : 'willia',
+//     idade 23
+
+//     console.log(cadastro);
+
+//     let JSON = JSON.stringify(cadastro);
+//     console.log(JSON);
+
+// }
